d9b69547d0f81c5dd583208da4aadcc3ed00ca71
Saúl Ruiz Fernández <saul.ruiz.30@ull.edu.es>
2024-04-16 16:28:28 +0100

    actualizadillo

--- catalogo.py
+++ catalogo.py
@@ -49,8 +49,13 @@ class Catalogo:
         """
         Busca las entradas de la tabla material que cumplen con el criterio de búsqueda.
         """
+        resultados = []
         for row in self.cursor.execute(f"SELECT * FROM material WHERE {campo} = '{valor}'"):
-            print(row)
+            resultados.append(str(row))
+
+        for resultado in resultados:
+            print(resultado)
+        return resultados
 
     def prestar(self, title, usuario):
         """
